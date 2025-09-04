5a47f234867e873c605c83c77a4c2a3c66e83572
Octocat& <3537256lol@gmail.com>
2025-09-04 13:12:44 +0700

    add examole code

---
+++ lab0/module1.h
@@ -0,0 +1,13 @@
+#ifndef MODULE1_H
+#define MODULE1_H
+
+#include <string>
+
+namespace Module1
+{
+    std::string getMyName();
+}
+
+
+
+#endif //MODULE1_H
---
+++ lab0/module2.h
@@ -0,0 +1,16 @@
+//
+// Created by kapiuser on 04.09.25.
+//
+
+#ifndef MODULE2_H
+#define MODULE2_H
+
+#include <string>
+
+namespace Module2
+{
+    std::string getMyName();
+}
+
+
+#endif //MODULE2_H
