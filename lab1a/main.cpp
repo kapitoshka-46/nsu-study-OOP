1ca4335f0617669baed360b99745a1f920663e21
Octocat& <3537256lol@gmail.com>
2025-09-21 15:22:40 +0700

    сравниваю STL реализацию со своей

--- lab1a/main.cpp
+++ lab1a/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <bitset>
 #include "BitArray.h"
 
 int main() {
-    auto bitarr = BitArray(8, 10);
+    constexpr int num_bits = 8;
+    constexpr int value = 11;
+
+
+    std::cout << "--- My realisation ---\n";
+    auto bitarr = BitArray(num_bits, value);
     std::cout << bitarr.to_string() << std::endl;
+
+
+    std::cout << "--- STL realisation ---\n";
+    auto cpparr = std::bitset<num_bits>(value);
+    std::cout << cpparr << std::endl;
     return 0;
 }
