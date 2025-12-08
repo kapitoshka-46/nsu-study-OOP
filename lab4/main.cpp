7e2b84eb608d71e24f0330ccb75f1133861db302
Octocat& <3537256lol@gmail.com>
2025-12-08 11:21:18 +0700

    Update main.cpp

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -2,7 +2,7 @@
 #include "tuple_extension.h"
 
 int main() {
-    std::tuple<int, float, double, std::string> t {3, 2.5f, 1.5f, "lol kek cheburek"};
+    std::tuple<int, float, double, std::string> t {3, 2.5f, 1.5f, "string"};
     std::cout << t;
     return 0;
 }
